ce0dd298f02410e9f65a1a32efe49ec357d93d8c
文煌 <wwh1004@hotmail.com>
2018-08-14 11:08:59 +0800

    v1.3.3.4

--- MinHook/Properties/AssemblyInfo.cs
+++ MinHook/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MinHook")]
 [assembly: AssemblyProduct("MinHook")]
 [assembly: AssemblyCopyright("Copyright © 2018 TsudaKageyu / Wwh")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.3.3.3")]
-[assembly: AssemblyFileVersion("1.3.3.3")]
+[assembly: AssemblyVersion("1.3.3.4")]
+[assembly: AssemblyFileVersion("1.3.3.4")]
