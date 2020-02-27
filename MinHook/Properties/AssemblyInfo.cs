afa561fb65a99e4da9b03ff6999bd5b852cd632d
文煌 <wwh1004@hotmail.com>
2020-02-27 11:03:53 +0800

    更新版本号和版权年份

--- MinHook/Properties/AssemblyInfo.cs
+++ MinHook/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("MinHook")]
 [assembly: AssemblyProduct("MinHook")]
-[assembly: AssemblyCopyright("Copyright © 2018-2019 TsudaKageyu / Wwh")]
-[assembly: AssemblyVersion("1.3.3.7")]
-[assembly: AssemblyFileVersion("1.3.3.7")]
+[assembly: AssemblyCopyright("Copyright © 2018-2020 TsudaKageyu / Wwh")]
+[assembly: AssemblyVersion("1.3.3.8")]
+[assembly: AssemblyFileVersion("1.3.3.8")]
